8b7bb7a5a6372c3b1097f5b380e7b906edf0a36c
Zeyad Abdel-Monem <zeyad.abmonem@gmail.com>
2026-09-26 00:34:41 +0300

    feat: add change password request dto

---
+++ src/Limita.Business/DTOs/Auth/ChangePasswordRequestDTO.cs
@@ -0,0 +1,15 @@
+namespace Limita.Business.DTOs.Auth;
+
+public class ChangePasswordRequestDTO
+{
+    [Required]
+    public string CurrentPassword { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(8)]
+    public string NewPassword { get; set; } = string.Empty;
+
+    [Required]
+    [Compare(nameof(NewPassword))]
+    public string ConfirmNewPassword { get; set; } = string.Empty;
+}
