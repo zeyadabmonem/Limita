11aca6494524873d4ed5fd2e4290852e270425d9
Zeyad Abdel-Monem <zeyad.abmonem@gmail.com>
2026-09-26 00:35:05 +0300

    feat: extend card repository contract

--- src/Limita.Data/Repo/Interface/ICardRepo.cs
+++ src/Limita.Data/Repo/Interface/ICardRepo.cs
@@ -1,10 +1,9 @@
-﻿namespace Limita.Data.Repo.Interface
+namespace Limita.Data.Repo.Interface;
+
+public interface ICardRepo
 {
-    public interface ICardRepo
-    {
-        Task<List<Card>> GetCardsByUserIdAsync(int userId);
-        Task<Card?> GetCardByIdAsync(int cardId);
-
-
-    }
+    Task<List<Card>> GetCardsByUserIdAsync(int userId);
+    Task<Card?> GetCardByIdAsync(int cardId);
+    Task AddAsync(Card card);
+    Task UpdateAsync(Card card);
 }
