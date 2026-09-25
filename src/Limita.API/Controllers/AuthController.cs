8558eabba6a9435ec38bd55bd1dad3df92c920d9
Zeyad Abdel-Monem <zeyad.abmonem@gmail.com>
2026-09-26 00:35:14 +0300

    feat: add change password endpoint

--- src/Limita.API/Controllers/AuthController.cs
+++ src/Limita.API/Controllers/AuthController.cs
@@ -6,32 +6,34 @@ public class AuthController : ControllerBase
 {
     private readonly IRegisterService registerService;
     private readonly ILoginService loginService;
+    private readonly IAuthService authService;
 
-    public AuthController(
-        IRegisterService registerService,
-        ILoginService loginService)
+    public AuthController(IRegisterService registerService, ILoginService loginService, IAuthService authService)
     {
         this.registerService = registerService;
         this.loginService = loginService;
+        this.authService = authService;
     }
 
     [HttpPost("register")]
-    public async Task<IActionResult> Register(
-        [FromBody] RegisterRequestDTO requestDTO)
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDTO requestDTO)
     {
-        ServiceResult<RegisterResponseDTO> result =
-            await registerService.RegisterAsync(requestDTO);
-
+        ServiceResult<RegisterResponseDTO> result = await registerService.RegisterAsync(requestDTO);
         return result.ToActionResult(this);
     }
 
     [HttpPost("login")]
-    public async Task<IActionResult> Login(
-        [FromBody] LoginRequestDTO requestDTO)
+    public async Task<IActionResult> Login([FromBody] LoginRequestDTO requestDTO)
     {
-        ServiceResult<LoginResponseDTO> result =
-            await loginService.LoginAsync(requestDTO);
+        ServiceResult<LoginResponseDTO> result = await loginService.LoginAsync(requestDTO);
+        return result.ToActionResult(this);
+    }
 
+    [Authorize]
+    [HttpPost("change-password")]
+    public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequestDTO requestDTO)
+    {
+        ServiceResult<bool> result = await authService.ChangePasswordAsync(User.GetUserId(), requestDTO);
         return result.ToActionResult(this);
     }
 }
